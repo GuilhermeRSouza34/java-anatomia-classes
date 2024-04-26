76d249b20649671f88659635abee5575bc6987b8
Guilherme <guilhermersouza34@gmail.com>
2024-04-26 12:44:37 -0300

    Utilizando .concat

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,5 +1,15 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        String primeiroNome = "Guilherme";
+        String segundoNome = "Rodrigues";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
